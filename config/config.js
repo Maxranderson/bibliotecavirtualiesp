696620a65a2a1c2ef5ee409bd3a75b013edb739a
Maxranderson Rodrigues <Maxranderson Rodrigues>
2017-09-19 21:29:37 -0300

    Criado a pasta public para os arquivos estaticos, movido tudo para public e configurado o servidor para usar essa pasta public. Controller respondendo a index

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var consign = require('consign');
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+app.use(express.static('public'));
 
 module.exports = function () {
     consign().include('./app/controllers').into(app);
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = require('./config/config.js')();
 var porta = 3000;
 
 app.listen(porta, function () {
-    console.log('Servidor rodando na porta: '+ porta)
+    console.log('Servidor rodando na porta: ' + porta);
 });
