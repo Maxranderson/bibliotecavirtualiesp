c73f1afa7ae9e08c1adf6a317af8183ef3ea4375
Maxranderson <max_rodrigues_araujo@hotmail.com>
2017-09-22 20:32:43 -0300

    Instalando e configurando novas dependências

--- config/config.js
+++ config/config.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 var consign = require('consign');
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
-app.use(express.static('public'));
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 module.exports = function () {
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views');
+    app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(session({secret: 'sssshhhh', resave:false, saveUninitialized: false}));
+
     consign().include('./app/controllers').into(app);
 
     return app;
