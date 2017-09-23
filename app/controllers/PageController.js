5d83d03f88d83ba74e72d998f9a4a59d28ddb786
Maxranderson <max_rodrigues_araujo@hotmail.com>
2017-09-23 14:25:32 -0300

    Template admin e primeira página do admin

--- app/controllers/PageController.js
+++ app/controllers/PageController.js
@@ -2,5 +2,9 @@ module.exports = function (server) {
     server.get('/', function (req, res) {
         var publicacao = new server.app.models.Publicacao();
         res.render("index");
-    })
+    });
+
+    server.get('/admin/publicacoes', function(req, res){
+        res.render('admin/publicacao/lista');
+    });
 }
