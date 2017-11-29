6b3e5bb0f7fc056515a2ffeaf2fcbc7d2a7fe338
Maxranderson <max_rodrigues_araujo@hotmail.com>
2017-11-29 17:41:46 -0300

    Corrigir migration e seed

--- database/migrations/20171031194938_create_table_users.js
+++ database/migrations/20171031194938_create_table_users.js
@@ -1,10 +1,11 @@
-
+const adm = require('../../config/config.json').administrador;
+const bcrypt = require('bcrypt-nodejs');
 exports.up = function (knex, Promise) {
     return knex.schema.createTableIfNotExists('users', function (table) {
         table.increments();
         table.string('username', 50).notNullable();
         table.text('password').notNullable();
-    }).then(function(table){ knex(table).insert({username: 'teste', password: '12345'});});
+    });
 }
 
 exports.down = function (knex, Promise) {
--- database/seeds/init/seeds/create_admin_user.js
+++ database/seeds/init/seeds/create_admin_user.js
@@ -1,7 +1,9 @@
+const adm = require('../../../../config/config.json').administrador;
 const bcrypt = require('bcrypt-nodejs');
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').insert([
-      {username: "admin", password: bcrypt.hashSync("testando_senh@", bcrypt.genSaltSync(8), null)}
+      {username: adm.username, password: bcrypt.hashSync(adm.senha, bcrypt.genSaltSync(8), null)}
     ]);
 };
